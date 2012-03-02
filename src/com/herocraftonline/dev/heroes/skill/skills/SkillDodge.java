18cc3ba96d561fef5382106e7fa5dd7876c49744
Multitallented <multitallented@pacbell.net>
2012-03-02 12:06:49 -0800

    maybe it was Dodge?

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillDodge.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillDodge.java
@@ -14,7 +14,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityListener;
 
 public class SkillDodge extends PassiveSkill {
 
