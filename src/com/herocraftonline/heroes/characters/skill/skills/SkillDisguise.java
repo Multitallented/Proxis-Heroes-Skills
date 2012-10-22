842915fcfe004d80c942b7f18f389a443420dace
Multitallented <daniel.miller@JINKIM-PC.sgi.int>
2012-10-22 16:21:10 -0700

    SkillDisguise is now compatible with DisguiseCraft and not MobDisguise

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillDisguise.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDisguise.java
@@ -152,10 +152,10 @@ public class SkillDisguise extends ActiveSkill {
             super.applyToHero(hero);
             Player player = hero.getPlayer();
             if (player.isOp()) {
-                player.performCommand("md " + mobtype);
+                player.performCommand("disguise " + mobtype);
             } else {
                 player.setOp(true);
-                player.performCommand("md " + mobtype);
+                player.performCommand("disguise " + mobtype);
                 player.setOp(false);
             }
             //broadcast(player.getLocation(), applyText, player.getDisplayName());
@@ -166,10 +166,10 @@ public class SkillDisguise extends ActiveSkill {
             super.removeFromHero(hero);
             Player player = hero.getPlayer();
             if (player.isOp()) {
-                player.performCommand("md");
+                player.performCommand("undisguise");
             } else {
                 player.setOp(true);
-                player.performCommand("md");
+                player.performCommand("undisguise");
                 player.setOp(false);
             }
         }
